3ddd3d0a0dec2f5af90bf4ca14786859a51147fb
yude <i@yude.jp>
2023-07-01 14:15:15 +0900

    feat: Determine the priority of nodes which is used for notification

--- routine/send_notify.go
+++ routine/send_notify.go
@@ -1,18 +1,42 @@
 package routine
 
 import (
+	"errors"
 	"log"
+	"sort"
 	"time"
 
+	Config "github.com/yude/kakashiz/config"
 	Event "github.com/yude/kakashiz/event"
 	Platform "github.com/yude/kakashiz/platform"
+	Status "github.com/yude/kakashiz/status"
+	Types "github.com/yude/kakashiz/types"
 )
 
-// Notify
+// SendNotify
 // Triggers the notification
 func SendNotify() {
+	me := Config.GetMe()
 	events := Event.GetEvents()
 	remote_events := Event.GetRemoteEvents()
+
+	// Determine the order of alive node slice
+	// The priority of node which is used for notification
+	// is the same as the name order of nodes (ascending order)
+	node_status := Status.GetNodeStatuses()
+	var alive_nodes []string
+	for k, v := range *node_status {
+		if v.Status == Types.Up {
+			alive_nodes = append(alive_nodes, k)
+		}
+	}
+	alive_nodes = append(alive_nodes, *me)
+	sort.SliceStable(alive_nodes, func(i, j int) bool {
+		return alive_nodes[i] < alive_nodes[j]
+	})
+	if len(alive_nodes) <= 0 {
+		errors.New("No node available to use for notification")
+	}
 
 	// eK: key of local event array
 	// eV: value of local event array
@@ -20,6 +44,16 @@ func SendNotify() {
 		// reK: key of remote event array
 		// reV: value of remote event array
 		for reK, reV := range *remote_events {
+			// Determine whether carries out the notification process
+			// about this event
+			// 1. Check if this event is older than 2 minutes
+			if eV.DateTime.Sub(time.Now()) <= time.Minute*2 {
+				// 2. Check if this node is the top batter
+				if alive_nodes[0] != *Config.GetMe() {
+					return
+				}
+			}
+
 			if eV.Name != reV.Event.Name { // Check the target node of this event
 				return
 			}
