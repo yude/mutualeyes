6bf2728595a804ab40fc3919c31a4fee1b7e3a7d
yude <i@yude.jp>
2023-07-07 16:42:04 +0900

    fix: Update routine schedule

--- routine/start.go
+++ routine/start.go
@@ -7,7 +7,7 @@ func Start() {
 
 	// The routines for production use.
 	c.AddFunc("@every 5s", func() { NodeAliveCheck() })
-	c.AddFunc("@every 5s", func() { SendNotify() })
+	c.AddFunc("@every 20s", func() { SendNotify() })
 
 	// The routines for debugging.
 	c.AddFunc("@every 5s", func() { PrintNodeEvents() })
