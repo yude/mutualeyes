643a51e62953a96f8af21301b8de3f0627898ba6
yude <i@yude.jp>
2023-08-21 20:32:36 +0900

    feat: マジックナンバーを保管

---
+++ constrants.py
@@ -0,0 +1,8 @@
+# 同じイベントであるとみなすことのできる時間差の最大値 (分)
+SAME_EVENT_TIME_LAG = 5
+
+# すべてのノードが特定のイベントを認知することを待機する時間 (分)
+EVENT_ACKNOWLEDGE_TIMEOUT = 6
+
+# 他のノードに通知の配信を交代させるまでの待機時間 (秒)
+EVENT_DELIVERY_TIMEOUT = 20
